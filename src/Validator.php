ee9a017e7bcb9ee7914217ecc46e6bacfa06ac9b
david-kurniawan <jatinangor-service@gmail.com>
2019-11-15 17:53:18 +0700

    more fixed

--- src/Validator.php
+++ src/Validator.php
@@ -2,24 +2,61 @@
 
 namespace Validvid\Validation;
 
-use Illuminate\Container\Container;
+use Illuminate\Validation;
+use Illuminate\Translation;
+use Illuminate\Validation\Factory;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Translation\FileLoader;
 use Illuminate\Translation\Translator;
-use Illuminate\Validation\Factory;
 
 class Validator
 {
-	public function make(array $data, array $rules)
-	{
-		try {
-		    $loader = new FileLoader(new Filesystem, 'lang');
-		    $translator = new Translator($loader, 'en');
-		    $validation = new Factory($translator, new Container);
+    public $lang;
+    public $group;
+    public $factory;
+    public $namespace;
+    public $basePath;
+    public static $translator;
 
-			return $validation->make($data, $rules);
-		} catch (\Exception $e) {
-			return $e->getMessage();
-		}
-	}
+    public function __construct($namespace = 'lang', $lang = 'en', $group = 'validation')
+    {
+        $this->lang = $lang;
+        $this->group = $group;
+        $this->namespace = $namespace;
+        $this->basePath = $this->getTranslationsRootPath();
+        $this->factory = new Factory($this->loadTranslator());
+    }
+
+    public function translationsRootPath(string $path = '')
+    {
+        if (!empty($path)) {
+            $this->basePath = $path;
+            $this->reloadValidatorFactory();
+        }
+        return $this;
+    }
+
+    private function reloadValidatorFactory()
+    {
+        $this->factory = new Factory($this->loadTranslator());
+        return $this;
+    }
+
+    public function getTranslationsRootPath() : string
+    {
+        return dirname(__FILE__) . '/';
+    }
+
+    public function loadTranslator() : Translator
+    {
+        $loader = new FileLoader(new Filesystem(), $this->basePath . $this->namespace);
+        $loader->addNamespace($this->namespace, $this->basePath . $this->namespace);
+        $loader->load($this->lang, $this->group, $this->namespace);
+        return static::$translator = new Translator($loader, $this->lang);
+    }
+
+    public function __call($method, $args)
+    {
+        return call_user_func_array([$this->factory, $method], $args);
+    }
 }
